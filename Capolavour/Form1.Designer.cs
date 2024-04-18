6252a1aeee7352e9b798c678d91131fd63f9484f
dridi.20189 <dridi.20189@LABI2-13PC15>
2024-04-18 10:30:09 +0200

    Aggiungere i file di progetto.

---
+++ Capolavour/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Capolavour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nome1 = new System.Windows.Forms.TextBox();
+            this.id1 = new System.Windows.Forms.TextBox();
+            this.cognome1 = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nome1
+            // 
+            this.nome1.Location = new System.Drawing.Point(56, 139);
+            this.nome1.Name = "nome1";
+            this.nome1.Size = new System.Drawing.Size(100, 20);
+            this.nome1.TabIndex = 0;
+            // 
+            // id1
+            // 
+            this.id1.Location = new System.Drawing.Point(290, 139);
+            this.id1.Name = "id1";
+            this.id1.Size = new System.Drawing.Size(100, 20);
+            this.id1.TabIndex = 1;
+            // 
+            // cognome1
+            // 
+            this.cognome1.Location = new System.Drawing.Point(175, 139);
+            this.cognome1.Name = "cognome1";
+            this.cognome1.Size = new System.Drawing.Size(100, 20);
+            this.cognome1.TabIndex = 2;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(56, 90);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(148, 23);
+            this.Add.TabIndex = 3;
+            this.Add.Text = "Aggiungi Giocatore";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(489, 60);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(229, 298);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(172, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cognome";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.cognome1);
+            this.Controls.Add(this.id1);
+            this.Controls.Add(this.nome1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nome1;
+        private System.Windows.Forms.TextBox id1;
+        private System.Windows.Forms.TextBox cognome1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Capolavour/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capolavour
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> Giocatori = new List<string>();
+        private string Nome, Cognome, id;
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            AddGiocatore();
+        }
+
+        public void AddGiocatore()
+        {
+            Giocatori.Add(nome1.Text);
+            listView1.Items.Add(nome1.Text);
+            Giocatori.Add(cognome1.Text);
+            listView1.Items.Add(cognome1.Text);
+            Giocatori.Add(id1.Text);
+            listView1.Items.Add(id1.Text);
+        }
+        
+    }
+}

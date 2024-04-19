17a9902938bbe2d4f035544c58ecf4a2a6362e09
dridi.20189 <dridi.ayoub.studente@itispaleocapa.it>
2024-04-19 13:37:15 +0200

    Rimuovi utente, migliorie sulla litsview

--- Capolavour/Form1.Designer.cs
+++ Capolavour/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // nome1
@@ -105,11 +106,22 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Cognome";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Rimuovi Giocatore";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -135,6 +147,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Capolavour/Form1.cs
+++ Capolavour/Form1.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 
 namespace Capolavour
@@ -18,22 +22,66 @@ namespace Capolavour
         }
 
         private List<string> Giocatori = new List<string>();
-        private string Nome, Cognome, id;
 
-        private void Add_Click(object sender, EventArgs e)
-        {
-            AddGiocatore();
-        }
+
+
 
         public void AddGiocatore()
         {
-            Giocatori.Add(nome1.Text);
-            listView1.Items.Add(nome1.Text);
-            Giocatori.Add(cognome1.Text);
-            listView1.Items.Add(cognome1.Text);
-            Giocatori.Add(id1.Text);
-            listView1.Items.Add(id1.Text);
+            string giocatore;
+            string Nome, Cognome, id;
+            Nome = nome1.Text; Cognome = cognome1.Text; id = id1.Text;
+            giocatore = Nome+" "+ Cognome + " " + id;
+            Nome.ElementA
+            Giocatori.Add(giocatore);
+            listView1.Items.Add(giocatore);
+
         }
-        
+        private void Add_Click(object sender, EventArgs e)
+        {
+            AddGiocatore();
+            nome1.Text = "";
+            cognome1.Text = "";
+            id1.Text = "";
+        }
+
+        public void DelGiocatore()
+        {
+            string giocatore;
+            string Nome, Cognome, id;
+            Nome = nome1.Text; Cognome = cognome1.Text; id = id1.Text;
+            giocatore = Nome + " " + Cognome + " " + id;
+            if (Giocatori.Contains(giocatore))
+            {
+                Giocatori.Remove(giocatore);
+
+            }
+            Visualizza();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DelGiocatore();
+            nome1.Text = "";
+            cognome1.Text = "";
+            id1.Text = "";
+
+        }
+
+        private void Visualizza()
+        {
+            listView1.Items.Clear();
+            for (int j = 0; j < Giocatori.Count; j++)
+                {
+                listView1.Items.Add(Giocatori[j]);
+                }
+        }
+
+
+        private void DEL_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
